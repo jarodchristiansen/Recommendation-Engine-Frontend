3e1c8b0fa16616a0163297d07713a85d52783169
Jarod Christiansen <jarodchristiansen2@gmail.com>
2024-09-28 15:19:21 -0700

    update nextauth to js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,12 @@
 // File: app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import { NextAuthOptions } from "next-auth";
 
-export const authOptions: NextAuthOptions = {
+export const authOptions = {
   providers: [
     SpotifyProvider({
-      clientId: process.env.SPOTIFY_CLIENT_ID!,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
       authorization:
         "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,playlist-modify-private,user-read-recently-played,user-top-read,user-library-read",
     }),
