bef3f60df4633b91d571f98f316d4f50cd118d4c
Jarod Christiansen <jarodchristiansen2@gmail.com>
2024-10-03 15:22:20 -0700

    Test updated route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,42 @@
-// File: app/api/auth/[...nextauth]/route.ts
+// // File: app/api/auth/[...nextauth]/route.ts
+// import NextAuth from "next-auth";
+// import SpotifyProvider from "next-auth/providers/spotify";
+
+// export const authOptions = {
+//   providers: [
+//     SpotifyProvider({
+//       clientId: process.env.SPOTIFY_CLIENT_ID,
+//       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+//       authorization:
+//         "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,playlist-modify-private,user-read-recently-played,user-top-read,user-library-read",
+//     }),
+//   ],
+//   callbacks: {
+//     async jwt({ token, account }) {
+//       if (account) {
+//         token.accessToken = account.access_token;
+//       }
+//       return token;
+//     },
+//     async session({ session }) {
+//       return session;
+//     },
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+//   session: {
+//     strategy: "jwt",
+//     maxAge: 3600 * 4, // 4 hour session duration
+//   },
+// };
+
+// const handler = NextAuth(authOptions);
+// export { handler as GET, handler as POST };
+
+// app/api/auth/[...nextauth]/route.js
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID,
@@ -25,9 +59,8 @@ export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
-    maxAge: 3600 * 4, // 4 hour session duration
+    maxAge: 3600 * 4,
   },
-};
+});
 
-const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
